3fa2409c4708549eafc5383cae55cdda94f5bffb
serjGavaleshko <s.gavaleshko@gmail.com>
2019-09-22 22:53:15 +0300

    Calc

--- src/ConsoleCalc.java
+++ src/ConsoleCalc.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ConsoleCalc {
+
+
+    public void insertion() {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first integer: ");
+        int int1 = in.nextInt();
+        System.out.print("Input second integer: ");
+        int int2 = in.nextInt();
+        System.out.print("Input +,-,/,*: ");
+        char char1 = in.next().charAt(0);
+
+        in.close();
+
+    }
+
+    public static int calc(int int1, int int2, char char1){
+
+        if (char1 == '+'){
+                int result11  = int1 + int2;
+        }else if(char1 == '-'){
+                 int result11  = int1 - int2;
+        }else if(char1 == '*'){
+            int result11  = int1 * int2;
+        }else if(char1 == '/'){
+            int result11  = int1 / int2;
+        }else
+        System.out.println("error");
+
+    }
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
       // new WideningCasting().widening();
-       new NarrowingCasting().narrowing();
+      // new NarrowingCasting().narrowing();
       // NarrowingCasting casting = new NarrowingCasting();
        //casting.narrowing();
+        new ConsoleCalc().insertion();
+
+      //  System.out.println(ConsoleCalc.insetion.result11);
 
     }
 }
