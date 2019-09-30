a91f24738456e7d1489ff8cacf66d89ab6aef1ac
serjGavaleshko <s.gavaleshko@gmail.com>
2019-09-30 17:11:01 +0200

    Frame_refactor_2

--- src/FrameDraw.java
+++ src/FrameDraw.java
@@ -4,16 +4,16 @@ public class FrameDraw {
         for (int i = 0; i < 5; i++) {
             if (i == 0 || i == 4) {
                 for (int l = 0; l < 5; l++) {
-                    System.out.print("*");
+                    System.out.print("* ");
                 }
                 System.out.println("");
 
             } else if (i == 1 || i == 2 || i == 3) {
-                for (int e = 0; e < 6; e++) {
-                    if (e == 0 || e == 4) {
+                for (int e = 0; e < 10; e++) {
+                    if (e == 0 || e == 8) {
                         System.out.print("*");
 
-                    } else if (e == 5)
+                    } else if (e == 9)
                         System.out.println("");
                     else
                         System.out.print(" ");
