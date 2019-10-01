ccd0acb4f0c310254227474901b9f85a6222fa54
serjGavaleshko <s.gavaleshko@gmail.com>
2019-10-01 17:55:03 +0200

    Triangle_refactor-1/10

--- src/HomeWork_4/TriangleDraw.java
+++ src/HomeWork_4/TriangleDraw.java
@@ -2,30 +2,18 @@ package HomeWork_4;
 
 public class TriangleDraw {
     public void drawing_t() {
-        String a0 = "* ";
-        String a1 = "* ";
-        String a2 = "* ";
-        String a3 = "* ";
-        String a4 = "* ";
+ int v = 50;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < v; i++) {
             System.out.println("");
-            if (i == 4) {
-                for (int a = 0; a < 5; a++) {
-                    System.out.println(a0 + a1 + a2 + a3 + a4);
-                }
-            } else if (i == 0) {
-                a0 = "  ";
-                a1 = "  ";
-                a4 = "  ";
-                a3 = "  ";
-                System.out.println(a0 + a1 + a2 + a3 + a4);
-            } else if (i == 1) {
-                a1 = "  ";
-                a4 = "  ";
-                a3 = "  ";
-                System.out.println(a0 + a1 + a2 + a3 + a4);
+            for (int a = 0; a < (2 * v); a++) {
+                if (a == v + i || a == v - i || a <= (v + i) && a >= (v - i) ) {
+                    System.out.print("* ");
+                } else  System.out.print("  ");
+                                         }
+                                    }
             }
         }
-    }
-}
+
+
+
