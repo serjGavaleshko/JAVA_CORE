b098c4aacadcc459b7d2359bcff7835845fdc47a
serjGavaleshko <s.gavaleshko@gmail.com>
2019-10-14 12:18:50 +0300

    Homework_6v1.3

--- src/Homework_6/PalindIdent.java
+++ src/Homework_6/PalindIdent.java
@@ -9,7 +9,8 @@ public class PalindIdent {
         System.out.print("Input string to check palindrome: ");
         String word1 = in.next();
         in.close();
-
+// Var_1
+        System.out.println("Var_1");
         word1 = word1.toLowerCase();
         char[] chars = word1.toCharArray();
         int p = 0;
@@ -19,7 +20,7 @@ public class PalindIdent {
             charsret[i] = chars[chars.length - 1 - i];
         }
         for (int j = 0; j < chars.length; j++) {
-            p = p + 1;
+            p++;
             if (chars[j] != charsret[j]) {
                 System.out.println("Your string is NOT palindrome!");
                 break;
@@ -28,7 +29,40 @@ public class PalindIdent {
         if (p == chars.length) {
             System.out.println("Your string is palindrome!");
         }
+
+ // Var_2
+        System.out.println("Var_2");
+
+        int ink = 0;
+        for (int k = 0; k < chars.length/2 ; k++) {
+            if (chars[k] == chars[chars.length-k-1]) {
+                  ink++;
+
+            } else { System.out.println("Your string is NOT palindrome!");
+            break;}
+
+        }
+        if (ink >= chars.length/2){
+        System.out.println("Your string is palindrome!");}
+
+// Var_3
+        System.out.println("Var_3");
+        for (int h = 0; h <chars.length ; h++) {
+            int check1 = Character.getNumericValue(chars[h]);
+            int check2 = Character.getNumericValue(charsret[h]);
+            int deltafinder = check1-check2;
+            if (deltafinder  == 0) {
+                System.out.println("Your string is palindrome!");
+                break;
+            } else {System.out.println("Your string is NOT palindrome!");
+                break;}
+
+
+        }
+
     }
+
+
 
 }
 
